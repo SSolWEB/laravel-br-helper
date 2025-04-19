4493903ebad0bf538e196615a9413f89f091c296
Vinicius de Santana <vinicius.vts@gmail.com>
2025-04-19 14:05:25 -0300

    feat: add data types to br phone caster

--- src/Casts/TelefoneCast.php
+++ src/Casts/TelefoneCast.php
@@ -4,10 +4,14 @@ namespace SSolWEB\LaravelBrHelper\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
+use SSolWEB\LaravelBrHelper\Enums\DBType;
+use SSolWEB\LaravelBrHelper\Traits\DBTypeTrait;
 use SSolWEB\StringMorpher\StringMorpher as SM;
 
 class TelefoneCast implements CastsAttributes
 {
+    use DBTypeTrait;
+
     /**
      * Transform the attribute from the underlying model values.
      *
@@ -19,7 +23,11 @@ class TelefoneCast implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return SM::maskBrPhone($value)
+        if (is_null($value)) {
+            return $value;
+        }
+        return SM::onlyNumbers($value)
+            ->maskBrPhone()
             ->getString();
     }
 
@@ -34,10 +42,14 @@ class TelefoneCast implements CastsAttributes
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        $value = SM::onlyNumbers($value);
-
-        return SM::onlyNumbers($value)
-            ->sub(0, 11)
-            ->getString();
+        if (is_null($value)) {
+            return $value;
+        }
+        return match ($this->dbType) {
+            DBType::INTEGER => (int) SM::onlyNumbers($value)->getString(),
+            DBType::FORMATTED => SM::onlyNumbers($value)->sub(0, 11)->maskBrPhone()->getString(),
+            // DBType::STRING is default
+            default => SM::onlyNumbers($value)->sub(0, 11)->getString(),
+        };
     }
 }
--- tests/Casts/TelefoneCastTest.php
+++ tests/Casts/TelefoneCastTest.php
@@ -5,6 +5,7 @@ namespace SSolWEB\LaravelBrHelper\Tests\Casts;
 use Illuminate\Database\Eloquent\Model;
 use Orchestra\Testbench\TestCase;
 use SSolWEB\LaravelBrHelper\Casts\TelefoneCast;
+use SSolWEB\LaravelBrHelper\Enums\DBType;
 
 class TelefoneCastTest extends TestCase
 {
@@ -50,6 +51,77 @@ class TelefoneCastTest extends TestCase
             protected $casts = ['telefone' => TelefoneCast::class];
         };
         $this->assertEquals('11999994444', $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+    }
+
+    public function testTelefoneCastAsDefault()
+    {
+        $model = new class extends Model {
+            protected function casts(): array
+            {
+                return ['telefone' => TelefoneCast::class];
+            }
+        };
+        $model->telefone = '(11) 99999-4444';
+        $this->assertEquals('11999994444', $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+        // tests fillable
+        $model = new class (['telefone' => '11999994444']) extends Model {
+            protected $fillable = ['telefone'];
+
+            protected function casts()
+            {
+                return ['telefone' => TelefoneCast::class];
+            }
+        };
+        $this->assertEquals('11999994444', $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+    }
+
+    public function testTelefoneCastAsString()
+    {
+        $model = new class extends Model {
+            protected function casts()
+            {
+                return ['telefone' => TelefoneCast::dbType(DBType::STRING)];
+            }
+        };
+        $model->telefone = '(11) 99999-4444';
+        $this->assertEquals('11999994444', $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+        // passing not formatted value
+        $model->telefone = '11999994444';
+        $this->assertEquals('11999994444', $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+    }
+
+    public function testTelefoneCastAsInteger()
+    {
+        $model = new class extends Model {
+            protected function casts()
+            {
+                return ['telefone' => TelefoneCast::dbType(DBType::INTEGER)];
+            }
+        };
+        $model->telefone = '(11) 99999-4444';
+        $this->assertEquals(11999994444, $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+        // passing integer value
+        $model->telefone = 11999994444;
+        $this->assertEquals(11999994444, $model->getAttributes()['telefone']);
+        $this->assertEquals('(11) 99999-4444', $model->telefone);
+    }
+
+    public function testTelefoneCastAsFormatted()
+    {
+        $model = new class extends Model {
+            protected function casts()
+            {
+                return ['telefone' => TelefoneCast::dbType(DBType::FORMATTED)];
+            }
+        };
+        $model->telefone = '11999994444';
+        $this->assertEquals('(11) 99999-4444', $model->getAttributes()['telefone']);
         $this->assertEquals('(11) 99999-4444', $model->telefone);
     }
 
